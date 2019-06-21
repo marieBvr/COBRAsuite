706ea5c4d3be6fc69b6dcc959edac8ca6e0d984f
marieBvr <marie.lefebvre@inra.fr>
2019-06-21 09:24:58 +0000

    Add SEARCH template

--- cobrasuite.py
+++ cobrasuite.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
