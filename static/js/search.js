44bcb263e0f67699966ecc6a4596613f8ebbcbef
marieBvr <lefebvre.m2@gmail.com>
2020-07-07 15:00:43 +0200

    Remove conseole log from search.js

--- static/js/search.js
+++ static/js/search.js
@@ -1,24 +1,11 @@
-$("#protein-form").submit(function(event){
-    // cancels the form submission
-    event.preventDefault();
-    console.log("form");
-    submitProteinForm();
-});
-
 function submitProteinForm(){
     // Initiate Variables With Form Content
     var protein = $("#search-protein").val();
     var species = $("#search-species").val();
-    console.log(protein, species);
- 
-    // $.ajax({
-    //     type: "POST",
-    //     url: "php/form-process.php",
-    //     data: "name=" + name + "&email=" + email + "&message=" + message,
-    //     success : function(text){
-    //         if (text == "success"){
-    //             formSuccess();
-    //         }
-    //     }
-    // });
 }
+
+$("#protein-form").submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+    submitProteinForm();
+});
